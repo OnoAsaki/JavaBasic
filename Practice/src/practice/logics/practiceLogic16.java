69c3e75fe18df11e4f9d815ea4c6a6df842362b3
大野朝希 <oonoasaki@oonoasamarenoMacBook-Pro.local>
2019-02-08 01:57:53 +0900

    09_02

---
+++ Practice/src/practice/logics/practiceLogic16.java
@@ -0,0 +1,16 @@
+package practice.logics;
+
+public class practiceLogic16 {
+
+	public static boolean checkChild (int age) {
+		boolean result;
+
+		if (age<20) {
+			result=true;
+		}else {
+			result=false;
+		}
+		return result;
+	}
+
+}
