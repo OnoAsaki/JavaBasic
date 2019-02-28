7d56f296f42d3cae527489415a5449e6f5cff7a8
大野朝希 <oonoasaki@oonoasamarenoMacBook-Pro.local>
2019-02-28 18:32:07 +0900

    18_01

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -16,5 +16,19 @@ public class PTra17_04 {
 		 * ※ Thread.sleep を使用すること
 		 */
 
+		char data[] = str.toCharArray();
+        int num = data.length;
+
+		for(int i = 0; i<= num-1; i++){
+			System.out.print(data[i]);
+			try{
+		        Thread.sleep(100);
+		    }
+		    catch(InterruptedException e){
+
+		    }
+
+		}
+
 	}
 }
